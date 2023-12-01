84ae1863f2d81e45af09906eedcdcd0e752d765e
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-12-01 01:41:52 +0100

    added hero section for the landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,20 @@
-import logo from "../assets/logo.svg";
+import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <header className="flex w-full items-center justify-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-10 h-16 pt-8">
-        <img src={logo} alt="Sumz logo" className="w-28 object-contain" />
-
-        <button
-          type="button"
-          onClick={() => window.open("https://github.com/ebukaeze")}
-          className="black_btn"
-        >
-          Github
-        </button>
-      </nav>
-      <div>
-        <h1 className="head_text">
+    <header className="flex w-full relative xs:justify-between items-center justify-center flex-col space-y-28 gap-y-10 h-[40dvh] md:h-[60vh]">
+      <Navbar />
+      <div className="w-full flex flex-col items-center justify-center h-[calc(40dvh - 56px)] max-w-screen-lg mx-auto text-center">
+        <h1 className="head_text text-6xl xs:text-2xl font-semibold">
           Summarize Articles with <br className="max-md:hidden" />
         </h1>
         <h2
-          className="head_text orange_gradient py-4 px-3 mt-4 bg-gradient-to-r from-pink-800 text-center 
-          to-orange-600 text-6xl text-white font-bold"
+          className="head_text orange_gradient xs:text-2xl md:py-4 px-3 md:mt-4 bg-gradient-to-r from-pink-800 text-center 
+          to-orange-600 text-3xl text-white font-bold md:text-6xl"
         >
-          {" "}
           Open AI GPT-4
         </h2>
-        <h3 className="desc">
+        <h3 className="desc xs:text-base">
           Simplify your reading with summize, an open-source
           <article>
             {" "}
