6c33a3a5e38320b2e32bedf7a16c106af755a203
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-12-01 01:34:39 +0100

    packages and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ export default {
       fontFamily: {
         satoshi: ["Satoshi", "sans-serif"],
         inter: ["Inter", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
       },
+    },
+    screens: {
+      xs: { max: "475px" },
+      // => @media (min-width: 640px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
